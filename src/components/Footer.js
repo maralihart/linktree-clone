a6a0d4335d4653e875d7e3ccba818937c0d8805e
Mara Hart <maralihart@gmail.com>
2021-12-02 20:15:20 -0800

    updated color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer({ information }) {
       background: colors.primary,
       color: colors.accent
     }}>
-      <p class="tiny" style={{ fontSize: "6pt", color: "inherit"}}>website designed & coded by <a href="https://mara.fyi">mara hart</a></p>
+      <p class="tiny" style={{ fontSize: "6pt", color: "inherit"}}>website designed & coded by <a href="https://mara.fyi" style={{ color: "inherit" }}><strong>mara hart</strong></a></p>
     </footer>
   );
 }
